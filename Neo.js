269e5b4c32aa409bf60c986cbebc4eaf867d5884
Samuel Silva <54758700+Samoody@users.noreply.github.com>
2026-03-18 13:30:12 -0300

    Create Neo.js

---
+++ Neo.js
@@ -0,0 +1,11 @@
+let idade = Number(prompt("Digite sua idade:"));
+let name = prompt("Digite seu nome:");
+
+if (idade >= 18) {
+    alert(`Parabéns ${name}, você é maior de idade!`);
+} else {
+    alert(`Desculpe ${name}, você é menor de idade!`);
+}
+
+console.log("Olá " + name + ", você tem " + idade + " anos!");
+console.log("Hi neo, Welcome to the matrix!");
