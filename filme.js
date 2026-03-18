7ae16d8433d409cfd31042c45bb382d8042b32e3
Samuel Silva <54758700+Samoody@users.noreply.github.com>
2026-03-18 18:29:47 -0300

    Create filme.js

---
+++ filme.js
@@ -0,0 +1,13 @@
+let idade = Number(prompt("Qual sua idade?"));
+let nome = prompt("Qual seu nome?");
+let terror = prompt("Qual seu filme de terror favorito?");
+
+if (idade < 18) {
+    console.log("Olá " + nome + ", você é menor de idade.");
+} else {
+    console.log("Olá " + nome + ", acesso liberado.");
+    console.log("Hi Neo, welcome to Matrix!");
+}
+
+
+console.log("Seu filme de terror favorito é: " + terror);
