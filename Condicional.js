8295eaa3589f45bb5fc15524f119b71b1879fada
Samuel Silva <54758700+Samoody@users.noreply.github.com>
2026-03-18 13:48:02 -0300

    Create Condicional.js

---
+++ Condicional.js
@@ -0,0 +1,11 @@
+let idade = prompt("Qual sua idade?");
+let nome = prompt("Qual seu nome?");
+if (idade <18){
+    console.log(" Olá " + nome + " Você é menor de idade , acesso negado! ");
+}
+    else{
+        console.log(" Olá " + nome + " Você é maior de idade, acesso permitido! ");
+        console.log(" Bem-vindo á Matrix!");
+        console.log(" Hi Neo, Welcome to the Matrix!");
+    
+}
